a7cdef2ea6f3381ce0366c3c4f7aad7c24fb2b0e
rodrigih <rh3rn4nd3z@gmail.com>
2024-07-11 15:14:36 -0700

    Fixed bug in get method. Added string method

--- grid.py
+++ grid.py
@@ -11,15 +11,37 @@ class Grid:
         self.grid = self.prepareGrid()
         self.configureCells()
 
+    def __str__(self):
+        output = "+" + ("---+" * self.columns) + "\n"
+
+        for row in self.eachRow():
+            top = "|"
+            bottom = "+"
+
+            for cell in row:
+                body = " " * 3
+                eastBoundary = " " if cell.isLinked(cell.east) else "|"
+                top = f"{top}{body}{eastBoundary}"
+
+                southBoundary = " " * 3 if cell.isLinked(cell.south) else "---"
+                bottom = f"{bottom}{southBoundary}+"
+
+            output = f"{output}{top}\n"
+            output = f"{output}{bottom}\n"
+
+        return output
+
     def get(self, row: int, col: int) -> Cell | None:
         """
         Gets Cell in specified row and column.
         If out of bounds, returns None
         """
-        try:
-            return self.grid[row][col]
-        except IndexError:
+        if row < 0 or row >= self.rows:
             return None
+        if col < 0 or col >= self.columns:
+            return None
+
+        return self.grid[row][col]
 
     def prepareGrid(self) -> list[list[Cell]]:
         """Creates 2-d array of cells"""
@@ -37,11 +59,8 @@ class Grid:
 
     def configureCells(self):
         """Tells each cell who it's neighbours are"""
-        rowRange = range(self.rows)
-        colRange = range(self.columns)
-
-        for row in rowRange:
-            for col in colRange:
+        for row in range(self.rows):
+            for col in range(self.columns):
                 cell = self.grid[row][col]
                 cell.north = self.get(row - 1, col)
                 cell.east = self.get(row, col + 1)
@@ -60,10 +79,12 @@ class Grid:
         return self.rows * self.columns
 
     def eachRow(self) -> Iterator[list[Cell]]:
+        """Returns Iterator for each row in Grid"""
         for row in self.grid:
             yield row
 
     def eachCell(self) -> Iterator[Cell]:
+        """Returns Iterator for each cell in Grid"""
         for rows in self.grid:
             for cell in rows:
                 yield cell
