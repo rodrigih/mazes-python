dffdb69d3a3eb887c5b8eeb6393a962b0d865aaa
rodrigih <rodrigih94@gmail.com>
2026-02-22 16:41:16 -0800

    Added customization to side winder algorithm

--- sideWinder.py
+++ sideWinder.py
@@ -1,30 +1,86 @@
 from grid import Grid
-from random import choice
+from enum import Enum
+from random import choice, random
 
 
-def on(grid: Grid):
-    """Sidewinder Algorithm for generating mazes
+class Texture(Enum):
+    SOUTH_TO_NORTH = ("passages go south to north",)
+    EAST_TO_WEST = "passages go east to west"
 
-    Given a grid, this function runs the Sidewinder algorithm to generate a maze
+
+class SideWinder:
+    """Class representing SideWinder algorithm
+
+    Attributes:
+        closeOutBias (float): Lower bias mean longer runs
+                              If texture south-to-north, this means wider runs
+                              If texture east-to-north, this means taller runs
+        texture (Texture): determines orientation paths tend to go
+
+    Methods:
+        on(grid): Generates a maze based on bias and texture provided
     """
-    for row in grid.eachRow():
-        run = []
 
-        for cell in row:
-            run.append(cell)
+    def __init__(self, closeOutBias=0.5, texture=Texture.SOUTH_TO_NORTH):
+        if texture in Texture:
+            self.texture = texture
+        else:
+            self.texture = Texture.SOUTH_TO_NORTH
 
-            isAtEasternBoundary = cell.east == None
-            isAtNorthernBoundary = cell.north == None
+        self.closeOutBias = closeOutBias
 
-            shouldCloseOut = isAtEasternBoundary or (
-                not isAtNorthernBoundary and choice([True, False])
-            )
+    def on(self, grid: Grid):
+        """
+        This is with
+        """
 
-            if shouldCloseOut:
-                chosenCell = choice(run)
-                if chosenCell.north:
-                    chosenCell.link(chosenCell.north)
-                    run.clear()
+        if self.texture == Texture.SOUTH_TO_NORTH:
+            self.withSouthToNorthTexture(grid)
+        else:
+            self.withEastToWestTexture(grid)
 
-            else:
-                cell.link(cell.east)
+    def withSouthToNorthTexture(self, grid):
+        for row in grid.eachRow():
+            run = []
+
+            for cell in row:
+                run.append(cell)
+
+                isAtEasternBoundary = cell.east is None
+                isAtNorthernBoundary = cell.north is None
+
+                shouldCloseOut = isAtEasternBoundary or (
+                    not isAtNorthernBoundary and random() < self.closeOutBias
+                )
+
+                if shouldCloseOut:
+                    chosenCell = choice(run)
+                    if chosenCell.north:
+                        chosenCell.link(chosenCell.north)
+                        run.clear()
+
+                else:
+                    cell.link(cell.east)
+
+    def withEastToWestTexture(self, grid):
+        for i in range(grid.columns):
+            run = []
+
+            for row in grid.eachRow():
+                cell = row[i]
+                run.append(cell)
+
+                isAtWesternBoundary = cell.west is None
+                isAtSouthernBoundary = cell.south is None
+
+                shouldCloseOut = isAtSouthernBoundary or (
+                    not isAtWesternBoundary and random() < self.closeOutBias
+                )
+
+                if shouldCloseOut:
+                    chosenCell = choice(run)
+                    if chosenCell.east:
+                        chosenCell.link(chosenCell.east)
+                        run.clear()
+                else:
+                    cell.link(cell.south)
--- sideWinderDemo.py
+++ sideWinderDemo.py
@@ -1,7 +1,8 @@
 from grid import Grid
-import sideWinder
+from sideWinder import SideWinder, Texture
 
 grid = Grid(4, 4)
+sideWinder = SideWinder(closeOutBias=0.5, texture=Texture.EAST_TO_WEST)
 sideWinder.on(grid)
 
 print(grid)
